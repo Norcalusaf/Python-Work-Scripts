866abb12ecf7802302955a6f774f783db6c052de
Chris Wilson <Norcalusaf04@hotmail.com>
2021-03-14 13:36:02 -0500

    updated comments and added in flush

--- Kafka - Docker/producer-consumer-scripts/producer.py
+++ Kafka - Docker/producer-consumer-scripts/producer.py
@@ -4,6 +4,10 @@ import json
 
 
 def send_data():
+    """
+    Sends data to a Kafka Topic
+    :return:
+    """
     producer = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x: dumps(x).encode('utf-8')
@@ -15,6 +19,7 @@ def send_data():
             producer.send('example_topic', zz)
             print(counter)
             counter += 1
+    producer.flush()
 
 
 if __name__ == "__main__":
